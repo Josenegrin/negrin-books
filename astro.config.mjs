d60bcff12f32638e0827806222f45c6dd38d147a
Jose Negrin <soyjosenegrin@gmail.com>
2023-08-01 20:56:38 +0200

    deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server'
-});
+	integrations: [tailwind()],
+	output: 'server',
+	site: 'https://josenegrin.github.io',
+	base: '/negrin-books'
+})
